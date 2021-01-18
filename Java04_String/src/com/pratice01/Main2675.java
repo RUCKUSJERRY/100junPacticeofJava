90392bae6a7a485073d2cd668f0a06bf6e9ffdaf
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2021-01-18 13:50:03 +0900

    20210118 2675 완료

--- Java04_String/src/com/pratice01/Main2675.java
+++ Java04_String/src/com/pratice01/Main2675.java
@@ -1,25 +1,50 @@
 package com.pratice01;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main2675 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
-		for (int i = 0; i < n; i++) {
-			String a = sc.next();
-			for (int j = 0; j < a.length(); j++) {
-				for (int j2 = 0; j2 < a.length(); j2++) {
-					System.out.print(a.charAt(j));
+
+		String a = "";
+
+		for (int i = 0; i < n * 2; i++) {
+
+			if (i % 2 == 0) {
+				a += sc.nextInt() + " ";
+			} else {
+				a += sc.next() + " ";
+			}
+
+		}
+
+		StringTokenizer st = new StringTokenizer(a, " ");
+		int tkn = st.countTokens();
+
+		for (int i = 0; i < tkn; i++) {
+			String token = "";
+			int cnt = 0;
+			String res = "";
+			if (i % 2 == 0) {
+				cnt = Integer.parseInt(st.nextToken());
+				token = st.nextToken();
+			} else {
+				continue;
+			}
+
+			for (int j = 0; j < token.length(); j++) {
+				for (int k = 0; k < cnt; k++) {
+					res += token.charAt(j);
 				}
 				
 			}
-			
+			System.out.println(res);
 		}
-		
+
 	}
-	
+
 }
