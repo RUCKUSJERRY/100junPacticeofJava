c80f6a09eaaa7df2b0c22b2b516a57b6da747dcb
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2021-02-14 18:30:33 +0900

    20210215 2869 complete

--- Java01_ifForWhile/src/com/test01/MainLessthan.java
+++ Java01_ifForWhile/src/com/test01/MainLessthan.java
@@ -3,27 +3,58 @@ package com.test01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class MainLessthan {
 
 	public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		// 유사 스캐너
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		// 토큰 문자열을 짤라서 넣어주는????
+//		int n = Integer.parseInt(st.nextToken());
+//		int m = Integer.parseInt(st.nextToken());
+//		// 입력한 값을 토큰에 넣어준다.
+//		st = new StringTokenizer(br.readLine());
+//		
+//		for (int i = 0; i < n; i++) {
+//			int tmp = Integer.parseInt(st.nextToken());
+//			
+//			if (tmp < m) {
+//				System.out.print(tmp + " ");
+//			}
+//		}
+//		br.close();
+//	}
 		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-
-		for (int i = 0; i < n; i++) {
-			int tmp = Integer.parseInt(st.nextToken());
-
-			if (tmp < m) {
-				System.out.print(tmp + " ");
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt(); // 10
+		int x = sc.nextInt(); // 5
+		
+		int []arr = new int[n];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			if (arr[i] < x) {
+				System.out.print(arr[i] + " ");
 			}
 		}
-		br.close();
+		
+//		for (int i = 0; i < n; i++) {
+//			int temp = sc.nextInt();
+//			// 1 10 4 9 2 3 8 5 7 6
+//			if (temp < x) {
+//				System.out.print(temp + " ");
+//			}
+//		}
+//		sc.close();
 	}
 
 }
--- Prs_level01/src/com/level01/gymClothes.java
+++ Prs_level01/src/com/level01/gymClothes.java
@@ -1,47 +1,53 @@
 package com.level01;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class gymClothes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int n = 10;
-		int [] lost = {1,3,6,9};
-		int [] reserve = {2,5,8};
+		int n = 5;
+		
+		int [] reserve = {1, 3, 5};
+		
+		int [] lost = {2, 4};
 		
 		solution(n, lost, reserve);
+		
+		
+		
 		
 	}
 
 	public static int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        answer = n - lost.length;
+        Set<Integer> set = new HashSet<Integer>();
         
+        answer = n - lost.length;
+        // 우선 잃어버린 학생을 빼준다.
         
         for (int i = 0; i < reserve.length; i++) {
-			for (int j = 0; j < lost.length; j++) {
-				if (reserve[i] == lost[i]) {
-					break;
-				} else if (reserve[i] == lost[i] - 1 || reserve[i] == lost[i] + 1){ 
-					if (reserve[i] == lost[i] - 1) {
-						System.out.println("reserve[i] : " + reserve[i]);
-						System.out.println("lost[i] - 1 : " + (lost[i] - 1));
-						answer++;
-						if (reserve[i+1] == lost[i] + 1) {
-							i++;
-						}
-						break;
-					} else if (reserve[i] == lost[i] + 1) {
-						System.out.println("reserve[i] : " + reserve[i]);
-						System.out.println("lost[i] + 1 : " + (lost[i] + 1));
-						answer++;
-						break;
-					}
-				}
+		// 여분있는 학생의 수만큼 반복하자.
+        	
+        	for (int j = 0; j < lost.length; j++) {
+				// reserve[i] 를 모든 reserve[j]와 비교
+        		if (reserve[i] - 1 == lost[j] || reserve[i] + 1 == lost[j]) {
+        			set.add(reserve[i]);
+        			if (reserve[i] - 1 == lost[j]) {
+        				
+        			} else if (reserve[i] + 1 == lost[j]) {
+        				
+        			}
+        		}
+        		
 			}
+        	
+        	
 		}
-        
         System.out.println(answer);
+        System.out.println(set.size());
         
         return answer;
     }
