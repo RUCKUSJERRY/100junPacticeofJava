1812581e2572a5cc5fa6ad5e080f7bfbc8492a85
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2021-02-17 00:16:52 +0900

    프로그래머스문자열 정리... sort빡세당..

---
+++ Prs_level01/src/com/level01/stringSort.java
@@ -0,0 +1,48 @@
+package com.level01;
+
+import java.util.Arrays;
+
+public class stringSort {
+
+	public static void main(String[] args) {
+		String[] a = { "aa", "bc", "bb", "ba", "cc" };
+		System.out.println(Arrays.toString(a));
+		Arrays.sort(a, 1, 3);
+		System.out.println(Arrays.toString(a));
+
+		String[] s = { "cba", "bac", "acb", "abc" , "bca", "cab" , "dab" , "baa"};
+		
+		int n = 1;
+
+		solution(s, n);
+
+	}
+
+	public static String[] solution(String[] strings, int n) {
+
+		String[] answer = new String[strings.length];
+
+		String[] temp = new String[strings.length];
+		
+		for (int i = 0; i < temp.length; i++) {
+			temp[i] = strings[i].charAt(n) + strings[i];
+		}
+		
+		System.out.println(Arrays.toString(temp));
+		Arrays.sort(temp);
+		System.out.println(Arrays.toString(temp));
+		
+		for (int i = 0; i < temp.length; i++) {
+			answer[i] = temp[i].substring(1);
+		}
+		
+//		Arrays.sort(answer);
+		System.out.println(Arrays.toString(answer));
+		return answer;
+	}
+//	temp = answer[j];
+//	answer[j] = answer[i];
+//	answer[i] = temp;
+	
+
+}
