97602891bf3f1a1f4abc372243aa7a0ffcc743db
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2021-02-18 14:02:12 +0900

    프로그래머스 연습문제 파스인트 완료

---
+++ Prs_level01/src/com/level01/parseInt.java
@@ -0,0 +1,17 @@
+package com.level01;
+
+public class parseInt {
+
+	public static void main(String[] args) {
+		int s = 5;
+	}
+	
+	public static int solution(String s) {
+        int answer = 0;
+        
+        answer = Integer.parseInt(s);
+        
+        return answer;
+    }
+	
+}
