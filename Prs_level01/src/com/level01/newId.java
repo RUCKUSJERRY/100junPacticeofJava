4f53a3d5e0088410c1c8e541f26c40a9dc65d2eb
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2021-04-26 13:36:52 +0900

    프로그래머스 / level1 / 신규 아이디 추천 / java

--- Prs_level01/src/com/level01/newId.java
+++ Prs_level01/src/com/level01/newId.java
@@ -14,7 +14,7 @@ public class newId {
 	public static String solution(String new_id) {
         String answer = "";
         String temp = "";
-	// 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+        // 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
         
        for (int i = 0; i < new_id.length(); i++) {
     	   
@@ -27,7 +27,7 @@ public class newId {
        }
        System.out.println("1단계 : " + answer);     
         
-	// 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+       // 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
        	for (int i = 0; i < answer.length(); i++) {      		
     	   switch (answer.charAt(i)) {
     	   case 'a':
@@ -155,7 +155,7 @@ public class newId {
        	answer = temp;  
        	temp = "";
        	System.out.println("2단계 : " + answer);   
-	// 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+       	// 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
        	for (int i = 0; i < answer.length(); i++) {
 			if (i >= 1 && answer.charAt(i) == '.' && answer.charAt(i-1) == '.') {
 				continue;
@@ -167,7 +167,7 @@ public class newId {
        	temp = "";
        	System.out.println("3단계 : " + answer);
        	
-	// 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+       	// 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
        	
        	for (int i = 0; i < answer.length(); i++) {
 			if (i == 0 && answer.charAt(i) == '.') {
@@ -182,13 +182,13 @@ public class newId {
        	answer = temp;
        	temp = "";
        	System.out.println("4단계 : " + answer);
-	// 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+       	// 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
        	if (answer == null || answer == "") {
        		
        		answer = "a";
        	}
        	System.out.println("5단계 : " + answer);
-	// 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+       	// 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
        	if (answer.length() > 15) {
        		for (int i = 0; i < 15; i++) {
 				temp += answer.charAt(i);
@@ -198,8 +198,7 @@ public class newId {
            	System.out.println("6단계 : " + answer);      	
        	}
        	
-    // 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-
+       	// 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
        	if (answer.charAt(answer.length()-1) == '.') {
        		for (int i = 0; i < answer.length()-1; i++) {
     				temp += answer.charAt(i);
@@ -209,7 +208,7 @@ public class newId {
            	System.out.println("6단계 : " + answer);   
        	}
        	   	
-	// 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+       	// 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
        	if (answer.length() == 2) {
        		temp += answer.charAt(0);
        		temp += answer.charAt(1);
