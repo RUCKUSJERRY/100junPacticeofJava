92b17f08972206623ccc39648851c8685a41031c
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2021-04-26 13:07:39 +0900

    프로그래머스 / level1 / 크레인 인형뽑기 게임 / java

--- Prs_level01/src/com/level01/crain.java
+++ Prs_level01/src/com/level01/crain.java
@@ -1,22 +1,6 @@
 package com.level01;
 
-import java.util.Arrays;
-import java.util.Iterator;
-
 public class crain {
-
-	/*
-	 * [0, 0, 0, 0, 0, 1], [0, 0, 1, 0, 3, 5], [0, 2, 5, 0, 1, 4], [4, 2, 4, 4, 2,
-	 * 4], [3, 5, 1, 3, 1, 6], [1, 2, 3, 4, 5, 6],
-	 * 
-	 * [0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 5], [0, 0, 5, 0, 0, 4], [0, 2, 4, 0, 2,
-	 * 4], [0, 5, 1, 3, 1, 6], [0, 2, 3, 4, 5, 6],
-	 * 
-	 * [1, 5, 3, 5, 1, 2, 1, 4, 6, 6, 6, 5]
-	 * 
-	 * [4, 3, 1, 1, 3, 2, 1, 4, 1, 5, 4, 2] [4, 2, 1, 4, 1, 5, 4, 2, 0, 0, 0, 0]
-	 * 
-	 */
 
 	public static void main(String[] args) {
 
@@ -27,68 +11,52 @@ public class crain {
 
 	}
 
-	/*
-	 * 0 1 2 3 4 [0,0,0,0,0] 0 [0,0,1,0,3] 1 [0,2,5,0,1] 2 [4,2,4,4,2] 3 [3,5,1,3,1]
-	 * 4
-	 */
-
-	// {1,5,3,5,1,2,1,4};
-	// 4 3 1 1 3 2 0 4
-	/*
-	 * 3 5 4 2 3
-	 * 
-	 */
-
 	public static int solution(int[][] board, int[] moves) {
 		int answer = 0;
-		int[] res = new int[moves.length]; // 바구니
-
+		int[] res = new int[moves.length];
+		// 바구니(res) 배열의 크기를 사용횟수(moves)와 동일하게 설정한다.
 		int cnt = 0;
 
-		// moves의 n번지의 번호를 가져온다.
 		for (int i = 0; i < moves.length; i++) {
+		// 사용횟수 만큼 반복문을 실행할 것 이다.
 
-			// 2차 n번지의 1차원 배열 값들을
 			for (int j = 0; j < board.length; j++) {
-
-				// 크레인에서 숫자 가져오기
+			// 그 다음 게임 화면의 행의 수 만큼 반복한다
+				
 				if (board[j][moves[i] - 1] != 0) {
+				// 만약에 게임화면의 [j] 번째 행의 [크레인이 집은 위치(moves[i]-1)] 가 0이 아니면
+	            // 0이 아니라는 것은 해당 위치에 숫자가 존재한다는 의미,
+	            // 숫자가 존재한다는 건 1~5 인형이 존재한다는 뜻으로 그 인형을 바구니에 담아준다.
+					
 					res[cnt] = board[j][moves[i] - 1];
+					// 해당 위치에 값이 바구니의 [cnt] 번째 값으로 초기화 되었다.
+					
 					if (cnt >= 1 && res[cnt] == res[cnt - 1]) {
+					// 만약에 바구니에 2개이상 담겨있으면서 (cnt >= 1)
+	                // 방금 담은 인형과 그 전에 담은 인형이 같은 인형이라면 res[cnt] == res[cnt - 1]
+						
 						res[cnt] = 0;
 						res[cnt - 1] = 0;
 						cnt -= 2;
 						answer += 2;
-
+						// 해당 인형 2개를 없애주고 사라진 인형개수를 return에 더해준다.
+						
 					}
+					
 					cnt++;
+					// 인형이 채워졌으니 넣을 칸을 한칸 올려준다
 					board[j][moves[i] - 1] = 0;
+					// 가져온 인형이 원래 있던 게임 화면에 인형을 제거한다.
 					break;
+					// 가져왔으니 반복문을 더 돌릴필요없으므로 멈추고 사용횟수로 넘어간다.
 				}
 				
-
 			}
 
 		}
 
-		System.out.println(Arrays.toString(res));
-
-		System.out.println(Arrays.toString(res));
-		System.out.println(Arrays.toString(board[0]));
-		System.out.println(Arrays.toString(board[1]));
-		System.out.println(Arrays.toString(board[2]));
-		System.out.println(Arrays.toString(board[3]));
-		System.out.println(Arrays.toString(board[4]));
-
-		// 그리고 해당 값을 res배열의 0번지 배열로 가져온다.
-		// 그리고 1번지 부터는 0번지 배열이랑 같은지 비교한 후에,
-		// 같으면 값을 넣지말고, 틀리면 값을 넣어준다.
-
-		// 그 다음 board의 n번지의 숫자들을 한칸씩 밑으로 내려준다.
-
-		// 반복
-		System.out.println(answer);
 		return answer;
+		// 결과출력
 	}
 
 }
