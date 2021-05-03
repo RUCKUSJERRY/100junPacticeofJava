3c6c88613b367f28dcb88b65709f1b56d510008e
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2021-05-03 14:20:53 +0900

    프로그래머스 연습문제 / level2 / 전화번호 목록 / 해시 / java

--- Prs_level01/src/com/level02/phoneNumList.java
+++ Prs_level01/src/com/level02/phoneNumList.java
@@ -8,27 +8,23 @@ public class phoneNumList {
 		String[] phone_book = {"119", "97674223", "1195524421"};
 		solution(phone_book);
 	}
-
+	
 	public static boolean solution(String[] phone_book) {
         
         boolean answer = true;
         
-       // Arrays.sort(phone_book);
-        // 배열을 오름차순대로 정렬
-        
-        for (int i = 0; i < phone_book.length; i++) {
-        	System.out.println(phone_book[i].length());
-        	for (int j = 0; j < phone_book.length; j++) {
-        		
-        		System.out.println(phone_book[j].substring(0, phone_book[i].length()));
-//				if(phone_book[i] == phone_book[j].substring(0, phone_book[i].length())) {
-//					
-//				}
-			}
-        	
+        Arrays.sort(phone_book);
+        // 숫자 오름차순으로 배열 정렬
+        System.out.println(Arrays.toString(phone_book));
+        for (int i = 0; i < phone_book.length-1; i++) {
+        	// 배열의 길이 -1 만큼 for문 반복
+        	if(phone_book[i+1].startsWith(phone_book[i])) {
+        		// i+1이 i의 값을 포함한다면 false 리턴하기
+        		answer = false;
+				break;
+        	}  	
 		}
-        
-        
+        System.out.println(answer); 
         return answer;
     }
 	
